596db419b9f76b04211427ab43b2f0bfe6fcd8e0
Aluno <aluno@IPIRANGA.FATEC>
2024-06-18 08:57:14 -0300

    feat(p2): aprovados com pais phds

--- script.sql
+++ script.sql
@@ -10,5 +10,25 @@ CREATE TABLE tb_student
   prep_exam   INTEGER
 );
 
+-- 2 Resultado em função da formação dos pais
+-- Escreva um stored procedure que exibe o número de alunos aprovados e cujos pais são
+-- ambos PhDs.
 
+CREATE OR REPLACE PROCEDURE sp_aprovados_phd(OUT resultado INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT (COUNT(*) FILTER (WHERE grade > 0)) FROM tb_student
+    WHERE mother_edu = 6 AND father_edu = 6
+    INTO resultado;
+END;
+$$
 
+DO $$
+DECLARE
+    resultado INT;
+BEGIN
+    CALL sp_aprovados_phd(resultado);
+    RAISE NOTICE 'Total de aprovados com ambos pais PHD: %', resultado;
+END;
+$$
